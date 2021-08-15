96ea26d384c82a8b7d21c3df154a3c65fc5cde2f
francisco souza <fsouza@users.noreply.github.com>
2021-08-14 23:05:15 -0400

    Implement MapError

--- channels.go
+++ channels.go
@@ -137,6 +137,52 @@ func FilterMap[InputType, OutputType any](ctx context.Context, in <-chan InputTy
 	return out
 }
 
+// MapError takes an input channel and a function to transform values of the
+// input type to some other type or an error, and returns two channels: one
+// with the output type and another one with errors. For each value consumed in
+// the input channel, the function is invoked and its results are processed as
+// follows:
+//
+//  - if the function does not return an error, sends the output of the
+//    function in the output channel.
+//  - if the function returns an error, send it to the error channel and
+//    ignores the other value.
+//
+// The capacity of the output channel will be same as the capacity of the input
+// channel. The capacity of the error channel will always be 0.
+//
+// This is a non-blocking function: it launches a goroutine and returns the
+// channel for consumption. In order to stop the inner goroutine, one can close
+// the input channel or cancel the provided context.
+//
+// The output and errors channels is always closed on cancellation, even if the
+// input channel is never closed.
+func MapError[InputType, OutputType any](ctx context.Context, in <-chan InputType, f func(InputType) (OutputType, error)) (<-chan OutputType, <-chan error) {
+	out := make(chan OutputType, cap(in))
+	errs := make(chan error)
+	go func() {
+		defer close(out)
+		defer close(errs)
+		for {
+			if v, ok := tryRead(ctx, in); ok {
+				var sent bool
+				if outValue, err := f(v); err != nil {
+					sent = trySend(ctx, errs, err)
+				} else {
+					sent = trySend(ctx, out, outValue)
+				}
+
+				if !sent {
+					return
+				}
+			} else {
+				return
+			}
+		}
+	}()
+	return out, errs
+}
+
 func max(x, y int) int {
 	if x > y {
 		return x
--- channels_test.go
+++ channels_test.go
@@ -184,7 +184,8 @@ func TestFilterWithContextCancellation(t *testing.T) {
 	}
 }
 
-// NOTE: Go is currently not happy with our FilterMap. Retry later.
+// NOTE: Go is currently not happy with our FilterMap nor MapError. Retry
+// later.
 
 // func TestFilterMap(t *testing.T) {
 // 	t.Parallel()
@@ -232,6 +233,38 @@ func TestFilterWithContextCancellation(t *testing.T) {
 // 	got := ToSlice(context.TODO(), lengths)
 // 	if len(got) == 0 {
 // 		t.Fatal("unexpected empty slice")
+// 	}
+// }
+
+// func TestMapError(t *testing.T) {
+// 	t.Parallel()
+// 	ch := startGenerator(t, 0, func(p int) (int, bool) {
+// 		return p + 1, true
+// 	}, nil)
+
+// 	doubledOdds, errs := MapError(context.TODO(), ch, func(v int) (int, error) {
+// 		var err error
+// 		if v%2 == 0 {
+// 			err = fmt.Errorf("%d is even, don't like that", v)
+// 		}
+// 		return v * 2, err
+// 	})
+
+// 	expectedVals := []int{2, 6, 10, 14, 18}
+// 	got := ToSlice(context.TODO(), Take(context.TODO(), doubledOdds, 5))
+// 	if !reflect.DeepEqual(got, expectedVals) {
+// 		t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expectedVals, got)
+// 	}
+
+// 	expectedErrs := []string{
+// 		"2 is even, don't like that",
+// 		"4 is even, don't like that",
+// 		"6 is even, don't like that",
+// 		"8 is even, don't like that",
+// 	}
+// 	gotErrs := ToSlice(context.TODO(), Map(context.TODO(), errs, func(err error) string { return err.Error() }))
+// 	if !reflect.DeepEqual(gotErrs, expectedErrs) {
+// 		t.Errorf("wrong errors returned\nwant %#v\ngot  %#v", expectedErrs, gotErrs)
 // 	}
 // }
 
