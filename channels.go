fed81ebeb885edcea0ff3ea13d3f1868e81d8e00
francisco souza <fsouza@users.noreply.github.com>
2021-08-14 22:09:50 -0400

    Change the meaning of Take
    Take still returns a channel, introduce ToSlice.
    
    ToSlice is probably useless, idk.

--- channels.go
+++ channels.go
@@ -2,17 +2,13 @@ package channels
 
 import "context"
 
-// Take takes at most n elements from the input channel and returns it in a
-// slice.
+// ToSlice converts the provided channel to a slice.
 //
 // This is a blocking function that can be aborted via the provided context or
 // by closing the input channel.
-func Take[T any](ctx context.Context, in <-chan T, n uint) []T {
+func ToSlice[T any](ctx context.Context, in <-chan T) []T {
 	var result []T
 	for {
-		if uint(len(result)) == n {
-			return result
-		}
 		select {
 		case v, ok := <-in:
 			if !ok {
@@ -23,6 +19,44 @@ func Take[T any](ctx context.Context, in <-chan T, n uint) []T {
 			return result
 		}
 	}
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// Take takes an input channel and returns an output channel that will contain
+// at most N elements from the input channel.
+//
+// The capacity of the output channel will be max(cap(inputChannel, n)).
+//
+// This is a non-blocking function: it launches a goroutine and returns the
+// channel for consumption. In order to stop the inner goroutine, one can close
+// the input channel or cancel the provided context.
+//
+// The output channel is always closed on cancellation or after sending N
+// elements, even if the input channel is never closed.
+func Take[T any](ctx context.Context, in <-chan T, n uint) <-chan T {
+	maxLen := int(n)
+	out := make(chan T, max(maxLen, cap(in)))
+	go func() {
+		defer close(out)
+		for i := 0; i < maxLen; i++ {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out
 }
 
 // Map takes an input channel and a function to transform values of the input
--- channels_test.go
+++ channels_test.go
@@ -7,6 +7,37 @@ import (
 	"time"
 )
 
+func TestToSlice(t *testing.T) {
+	t.Parallel()
+	ch := startGenerator(t, 0, func(p int) (int, bool) {
+		if p > 4 {
+			return p, false
+		}
+		return p + 1, true
+	}, nil)
+
+	values := ToSlice(context.TODO(), ch)
+	expectedSlice := []int{1, 2, 3, 4, 5}
+	if !reflect.DeepEqual(values, expectedSlice) {
+		t.Errorf("wrong slice returned\nwant %#v\ngot  %#v", expectedSlice, values)
+	}
+}
+
+func TestToSliceWithContextCancellation(t *testing.T) {
+	t.Parallel()
+	ch := startGenerator(t, "", func(v string) (string, bool) {
+		return v, true
+	}, func() { time.Sleep(time.Second) })
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+	values := ToSlice(ctx, ch)
+	expectedSlice := []string(nil)
+	if !reflect.DeepEqual(values, expectedSlice) {
+		t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expectedSlice, values)
+	}
+}
+
 func TestTake(t *testing.T) {
 	t.Parallel()
 	ch := startGenerator(t, 0, func(p int) (int, bool) {
@@ -17,25 +48,25 @@ func TestTake(t *testing.T) {
 	}, nil)
 
 	t.Run("takes only the number of arguments", func(t *testing.T) {
-		values := Take(context.TODO(), ch, 5)
+		values := ToSlice(context.TODO(), Take(context.TODO(), ch, 5))
 		expectedSlice := []int{1, 2, 3, 4, 5}
 		if !reflect.DeepEqual(values, expectedSlice) {
-			t.Errorf("wrong slice returned by Take(5)\nwant %#v\ngot  %#v", expectedSlice, values)
+			t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expectedSlice, values)
 		}
 	})
 
 	t.Run("can take 0 elements", func(t *testing.T) {
-		values := Take(context.TODO(), ch, 0)
+		values := ToSlice(context.TODO(), Take(context.TODO(), ch, 0))
 		if values != nil {
 			t.Errorf("unexpected non-nil slice from Take(0): %#v", values)
 		}
 	})
 
 	t.Run("can take less than N if channel is closed", func(t *testing.T) {
-		values := Take(context.TODO(), ch, 10)
+		values := ToSlice(context.TODO(), Take(context.TODO(), ch, 10))
 		expectedSlice := []int{6, 7, 8, 9, 10}
 		if !reflect.DeepEqual(values, expectedSlice) {
-			t.Errorf("wrong slice returned by Take(5)\nwant %#v\ngot  %#v", expectedSlice, values)
+			t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expectedSlice, values)
 		}
 	})
 }
@@ -48,10 +79,10 @@ func TestTakeWithContextCancellation(t *testing.T) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 	defer cancel()
-	values := Take(ctx, ch, 5)
+	values := ToSlice(context.TODO(), Take(ctx, ch, 5))
 	expectedSlice := []string(nil)
 	if !reflect.DeepEqual(values, expectedSlice) {
-		t.Errorf("wrong slice returned by Take(5)\nwant %#v\ngot  %#v", expectedSlice, values)
+		t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expectedSlice, values)
 	}
 }
 
@@ -64,7 +95,7 @@ func TestMap(t *testing.T) {
 	doubled := Map(context.TODO(), ch, func(v int) int { return v * 2 })
 
 	expected := []int{2, 4, 6, 8, 10}
-	got := Take(context.TODO(), doubled, 5)
+	got := ToSlice(context.TODO(), Take(context.TODO(), doubled, 5))
 	if !reflect.DeepEqual(got, expected) {
 		t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expected, got)
 	}
@@ -82,7 +113,7 @@ func TestMapWithClosedInputChannel(t *testing.T) {
 	doubled := Map(context.TODO(), ch, func(v int) int { return v * 2 })
 
 	expected := []int{2, 4, 6, 8}
-	got := Take(context.TODO(), doubled, 1e9)
+	got := ToSlice(context.TODO(), doubled)
 	if !reflect.DeepEqual(got, expected) {
 		t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expected, got)
 	}
@@ -98,7 +129,7 @@ func TestMapWithContextCancellation(t *testing.T) {
 	defer cancel()
 	lengths := Map(ctx, ch, func(v string) int { return len(v) })
 
-	got := Take(context.TODO(), lengths, 1e9)
+	got := ToSlice(context.TODO(), lengths)
 	if len(got) == 0 {
 		t.Fatal("unexpected empty slice")
 	}
