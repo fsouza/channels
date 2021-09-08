c5db73e67dcdfebf3e636ef1e72851b3ee671344
francisco souza <fsouza@users.noreply.github.com>
2021-09-07 22:55:25 -0400

    Fix tests

--- channels_test.go
+++ channels_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"sync"
 	"testing"
 	"time"
 )
@@ -203,7 +204,7 @@ func TestFilterMap(t *testing.T) {
 func TestFilterMapWithClosedInputChannel(t *testing.T) {
 	t.Parallel()
 	ch := startGenerator(t, 0, func(p int) (int, bool) {
-		if p > 3 {
+		if p > 8 {
 			return p, false
 		}
 		return p + 1, true
@@ -241,17 +242,31 @@ func TestMapError(t *testing.T) {
 	}, nil)
 
 	doubledOdds, errs := MapError(context.TODO(), ch, func(v int) (int, error) {
-		var err error
 		if v%2 == 0 {
-			err = fmt.Errorf("%d is even, don't like that", v)
+			return 0, fmt.Errorf("%d is even, don't like that", v)
 		}
-		return v * 2, err
+		return v * 2, nil
 	})
 
+	var gotVals []int
+	var gotErrs []string
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		gotVals = ToSlice(context.TODO(), Take(context.TODO(), doubledOdds, 5))
+	}()
+
+	go func() {
+		defer wg.Done()
+		gotErrs = ToSlice(context.TODO(), Map(context.TODO(), Take(context.TODO(), errs, 5), func(err error) string { return err.Error() }))
+	}()
+	wg.Wait()
+
 	expectedVals := []int{2, 6, 10, 14, 18}
-	got := ToSlice(context.TODO(), Take(context.TODO(), doubledOdds, 5))
-	if !reflect.DeepEqual(got, expectedVals) {
-		t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expectedVals, got)
+	if !reflect.DeepEqual(gotVals, expectedVals) {
+		t.Errorf("wrong values returned\nwant %#v\ngot  %#v", expectedVals, gotVals)
 	}
 
 	expectedErrs := []string{
@@ -259,8 +274,8 @@ func TestMapError(t *testing.T) {
 		"4 is even, don't like that",
 		"6 is even, don't like that",
 		"8 is even, don't like that",
+		"10 is even, don't like that",
 	}
-	gotErrs := ToSlice(context.TODO(), Map(context.TODO(), errs, func(err error) string { return err.Error() }))
 	if !reflect.DeepEqual(gotErrs, expectedErrs) {
 		t.Errorf("wrong errors returned\nwant %#v\ngot  %#v", expectedErrs, gotErrs)
 	}
